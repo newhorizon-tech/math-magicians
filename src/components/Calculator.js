af516a2d4d529d73dbdfca03f2237dd36e4478b7
newhorizon-tech <alaukikinbox@gmail.com>
2021-10-23 03:28:33 +0530

    Refactor Calculator component to use hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,45 +1,41 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Row from './Row';
 import './Calculator.css';
 
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { };
-  }
+const Calculator = () => {
+  const [calcObj, setCalcObj] = useState({});
+  const { total } = calcObj;
 
-  handleCalc = (e) => {
+  const handleCalc = (e) => {
     const buttonName = e.target.textContent;
-    const newState = calculate(this.state, buttonName);
-    this.setState(Object.assign(newState));
-  }
+    const newState = calculate(calcObj, buttonName);
+    setCalcObj(Object.assign(newState));
+  };
 
-  render() {
-    const { total } = this.state;
-    const buttons = [
-      ['AC', '+/-', '%', '/'],
-      ['7', '8', '9', 'x'],
-      ['4', '5', '6', '-'],
-      ['1', '2', '3', '+'],
-      ['0', '.', '='],
-    ];
-    return (
-      <div id="calculator">
-        <div id="display-calc">
-          {total}
-        </div>
+  const buttons = [
+    ['AC', '+/-', '%', '/'],
+    ['7', '8', '9', 'x'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '.', '='],
+  ];
 
-        <div id="calc-buttons">
-          {buttons.map((row) => (
-            <Row key={row} rowButtons={row} handleCalc={() => this.handleCalc} />
-          ))}
-        </div>
+  return (
+    <div id="calculator">
+      <div id="display-calc">
+        {total}
       </div>
 
-    );
-  }
-}
+      <div id="calc-buttons">
+        {buttons.map((row) => (
+          <Row key={row} rowButtons={row} handleCalc={() => handleCalc} />
+        ))}
+      </div>
+    </div>
+
+  );
+};
 
 export default Calculator;
