22ddf53e70f1e103136b9a0abdfe576652c97d95
newhorizon-tech <alaukikinbox@gmail.com>
2021-10-23 02:46:35 +0530

    Refactor Row component

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,24 +1,17 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-class Row extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Row = (props) => {
+  const { rowButtons, handleCalc } = props;
+  return (
+    <div className="row">
+      {rowButtons.map((name) => (
+        <Button key={name} buttonName={name} handleCalc={handleCalc} />
+      ))}
+    </div>
+  );
+};
 
-  render() {
-    const { rowButtons, handleCalc } = this.props;
-    return (
-      <div className="row">
-        {rowButtons.map((name) => (
-          <Button key={name} buttonName={name} handleCalc={handleCalc} />
-        ))}
-      </div>
-    );
-  }
-}
 Row.propTypes = {
   rowButtons: PropTypes.arrayOf(PropTypes.string).isRequired,
   handleCalc: PropTypes.func.isRequired,
