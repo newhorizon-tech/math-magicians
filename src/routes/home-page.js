46d4d03d89d831510560540fb7d66713b7a3ed44
newhorizon-tech <alaukikinbox@gmail.com>
2021-10-27 22:20:36 +0530

    Add content to home page

--- src/routes/home-page.js
+++ src/routes/home-page.js
@@ -1,5 +1,28 @@
 const HomePage = () => (
-  <h1> Home Page </h1>
+  <article>
+    <h3>
+      Welcome to our page!
+    </h3>
+    <p>
+      Mathematics, the science of structure, order, and relation that has
+      evolved from elemental practices of counting, measuring, and describing
+      the shapes of objects.It deals with logical reasoning and quantitative
+      calculation, and its development has involved an increasing degree of
+      idealization and abstraction of its subject matter.Since the 17th century,
+      mathematics has been an indispensable adjunct to the physical sciences and
+      technology, and in more recent times it has assumed a similar role in the
+      quantitative aspects of the life sciences.
+    </p>
+
+    <p>
+      In many cultures— under the stimulus of the needs of practical pursuits,
+      such as commerce and agriculture— mathematics has developed far beyond
+      basic counting.This growth has been greatest in societies complex enough
+      to sustain these activities and to provide leisure for contemplation and
+      the opportunity to build on the achievements of earlier mathematicians.
+    </p>
+
+  </article>
 );
 
 export default HomePage;
