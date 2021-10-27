d49769e1fe3a3e71dd58f246e3e41aae2950636f
newhorizon-tech <alaukikinbox@gmail.com>
2021-10-27 22:56:29 +0530

    Fix home page content

--- src/routes/home-page.js
+++ src/routes/home-page.js
@@ -1,12 +1,12 @@
 const HomePage = () => (
   <article className="page-content">
-    <h3>
+    <h2>
       Welcome to our page!
-    </h3>
+    </h2>
     <p>
-      Mathematics, the science of structure, order, and relation that has
+      Mathematics is the science of structure, order, and relation that has
       evolved from elemental practices of counting, measuring, and describing
-      the shapes of objects.It deals with logical reasoning and quantitative
+      the shapes of objects. It deals with logical reasoning and quantitative
       calculation, and its development has involved an increasing degree of
       idealization and abstraction of its subject matter.Since the 17th century,
       mathematics has been an indispensable adjunct to the physical sciences and
@@ -15,9 +15,9 @@ const HomePage = () => (
     </p>
 
     <p>
-      In many cultures— under the stimulus of the needs of practical pursuits,
-      such as commerce and agriculture— mathematics has developed far beyond
-      basic counting.This growth has been greatest in societies complex enough
+      In many cultures — under the stimulus of the needs of practical pursuits,
+      such as commerce and agriculture — mathematics has developed far beyond
+      basic counting. This growth has been greatest in societies complex enough
       to sustain these activities and to provide leisure for contemplation and
       the opportunity to build on the achievements of earlier mathematicians.
     </p>
