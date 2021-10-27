badda0c98e76d023f27b534fd3dd623af59a0faa
newhorizon-tech <alaukikinbox@gmail.com>
2021-10-27 22:52:10 +0530

    Add quote

--- src/routes/quotes-page.js
+++ src/routes/quotes-page.js
@@ -1,5 +1,19 @@
+import './quotes.css';
+
 const QuotesPage = () => (
-  <h1> Quotes </h1>
+  <figure className="quote page-content">
+    <blockquote>
+      Our experience up to date justifies us in feeling sure that in
+      Nature is actualized the ideal of mathematical simplicity.
+      It is my conviction that pure mathematical construction enables
+      us to discover the concepts and the laws connecting them, which
+      gives us the key to understanding nature.In a certain sense, therefore,
+      I hold it true that pure thought can grasp reality, as the ancients dreamed.
+    </blockquote>
+    <figcaption>
+      &mdash; Albert Einstein
+    </figcaption>
+  </figure>
 );
 
 export default QuotesPage;
