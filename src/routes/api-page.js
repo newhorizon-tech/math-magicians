e429c55a48d0eba002172c5f3416911e8493673c
newhorizon-tech <alaukikinbox@gmail.com>
2021-11-25 19:08:12 +0530

    Add API handling

--- src/routes/api-page.js
+++ src/routes/api-page.js
@@ -1,25 +1,36 @@
 import { useState } from 'react';
 
 const ApiPage = () => {
-  // const result = 5;
-  const [result, setResult] = useState({ value: 5 });
-  const handleResult = (e) => {
+  const [result, setResult] = useState({});
+
+  const handleData = async (exp) => {
+    const response = await fetch(`http://api.mathjs.org/v4/?expr=${encodeURIComponent(exp)}`);
+    console.log(response);
+    if (response.status === 200) {
+      const apiResult = await response.json();
+      setResult((prevState) => ({ ...prevState, value: apiResult }));
+    } else {
+      setResult((prevState) => ({ ...prevState, error: 'Invalid Input' }));
+    }
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
     const input = form.expression.value;
+    handleData(input);
     form.reset();
-    setResult((prevState) => ({ value: prevState.value + parseInt(input, 10) }));
   };
 
   return (
     <div>
       <h1> API </h1>
-      <form onSubmit={(e) => handleResult(e)}>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input id="expression" placeholder="Enter math expression" />
         <button type="submit"> Calculate </button>
       </form>
       <p id="result">
-        {result.value}
+        {result.error || result.value}
       </p>
     </div>
   );
